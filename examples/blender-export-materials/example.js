ab1fbf7b8d49b333566d68af5bf7c5cd855a7c83
humbletim <humbletim@gmail.com>
2016-10-21 16:06:08 -0400

    add support for dynamic procedural entity parameters to example

--- examples/blender-export-materials/example.js
+++ examples/blender-export-materials/example.js
@@ -1,49 +1,99 @@
-Script.include('https://cdn.rawgit.com/humbletim/hifi-aux/5dfa4d68/snippets/extract-parameters.js');
+// creates a ring of ProceduralEntity Shapes around MyAvatar
+//
+// to load a custom shader with this script, use one of the following URL formats:
+//
+// * Custom shaderUrl:
+//   `example.js?shaderUrl=...`
+//
+// * Custom shaderUrl with addition properties:
+//   `example.js?shaderUrl=...&channels=["http://path/to/image.png"]`
+//
+// * Custom ProceduralEntity JSON:
+//   `example.js?ProceduralEntity={"version":2,"shaderUrl":"...","uniforms":{},"channels":[]}`
+//
+// by default all available Shape types (Sphere, Cube, Tetrahedron, etc.) will be created
+// ... to limit to just one shape (or a few) add `&shapes=Cube` or `&shapes=cube,tetra,ico` etc.
+//     (only the first few unique letters of a shape need to be specified)
+//
+//     -- humbletim @ 2016.10.21
+//
+
+Script.include('https://cdn.rawgit.com/humbletim/hifi-aux/194d418a/snippets/extract-parameters.js');
 try { throw new Error('stacktrace'); } catch(e) { var kvargs = extractParameters(e.fileName); }
 
-var PROCEDURAL = 2;
-var shaderUrl = Script.resolvePath( kvargs.frag || 'mat_ShinyBlue.hifi.fs' );
+kvargs.shapes = kvargs.shapes || kvargs.shape;
 
-print('shaderUrl', shaderUrl);
+var userData = kvargs.$object.ProceduralEntity || {
+    ProceduralEntity: {
+        version: kvargs.version,
+        shaderUrl: kvargs.shaderUrl || kvargs.frag,
+        uniforms: kvargs.uniforms,
+        channels: kvargs.channels
+    }
+};
+
+userData.ProceduralEntity.shaderUrl =
+    userData.ProceduralEntity.shaderUrl || 'mat_ShinyBlue.hifi.fs';
+
+if (!userData.ProceduralEntity.version) {
+    // attempt to look up procedural version via heuristics
+    print('example.js | loading ' + userData.ProceduralEntity.shaderUrl + ' to determine version...');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', userData.ProceduralEntity.shaderUrl, false);
+    xhr.send();
+    if (!xhr.responseText)
+        print('example.js | error: could not load ' + userData.ProceduralEntity.shaderUrl);
+    userData.ProceduralEntity.version = /getProceduralColors/.test(xhr.responseText) ? 2 :
+        /getProceduralColor/.test(xhr.responseText) ? 1 : 2;
+}
+
+print('example.js | userData', JSON.stringify(userData,0,2));
 
 function mkent(shape, offset) {
-    return Entities.addEntity({
-        name: shape + ' GLSL test',
+    var props = {
+        name: '[GLSL test] ' + shape + ' w/' +
+            userData.ProceduralEntity.shaderUrl.split(/[#?]/)[0].split('/').pop(),
         lifetime: 600,
+        clientOnly: !(Entities.canRezTmp()||Entities.canRez()),
         type: 'Shape',
         shape: shape,
-        position: Vec3.sum(MyAvatar.position, offset||Vec3.ZERO),
+        position: Vec3.sum(MyAvatar.position, offset || Vec3.ZERO),
         dimensions: Vec3.ONE,
-        color: { red: 255, green: 255, blue: 255 },
+        color: mkent.first ? { red: 255, green: 255, blue: 255 } : (mkent.first = {red:0,green:255,blue:0}),
         collisionless: true,
         angularDamping: 0.0,
         angularVelocity: Vec3.multiply(1.0, {x: Math.random(), y: Math.random(), z: Math.random()}),
-        userData: JSON.stringify({
-            "ProceduralEntity": {
-                "version": PROCEDURAL,
-                "shaderUrl": shaderUrl
-            }
-        }),
+        userData: JSON.stringify(userData),
         script: '('+function() {
             return {
-                clickDownOnEntity: function(uuid) {
-                    var props = Entities.getEntityProperties(uuid),
-                        data = JSON.parse(props.userData),
+                clickDownOnEntity: function(uuid, evt) {
+                    var props = Entities.getEntityProperties(uuid);
+                    if (evt.button !== 'Tertiary')
+                        return print('example.js | clickDownOnEntity | ' + props.name);
+
+                    var data = JSON.parse(props.userData),
                         proc = data.ProceduralEntity,
                         frag = proc.shaderUrl.split('#')[0];
                     
                     proc.version = proc.version == 2 ? 1 : 2;
-                    print('... swithing '+props.name+' to PROCEDURAL_V'+proc.version);
+                    print('example.js | ... swithing '+props.name+' to PROCEDURAL_V'+proc.version);
                     proc.shaderUrl = frag + '#' + new Date().getTime();
-                    print('data', uuid, props.shape, JSON.stringify(data));
+                    print('example.js | data', uuid, props.shape, JSON.stringify(data));
                     Entities.editEntity(uuid, { userData: JSON.stringify(data) });
                 },
             };
         }+')',
-    }, !(Entities.canRezTmp()||Entities.canRez()));
+    };
+    var uuid = Entities.addEntity(props, props.clientOnly);
+    var aprops = Entities.getEntityProperties(uuid);
+    if (aprops.id !== uuid)
+        print('example.js | failed to create entity...', props.name);
+    else
+        print('example.js | created', props.name, '@', JSON.stringify(props.position));
+    return uuid;
 }
 
-var uuids = [
+var SHAPES = [
     "Triangle",
     //"Quad",
     "Hexagon",
@@ -58,10 +108,31 @@ var uuids = [
     //"Torus",
     //"Cone",
     //"Cylinder"
-    
-].map(function(shape,i,arr) {
-    var t = i/(arr.length)*Math.PI*2;
-    return mkent(shape, Quat.multiply(Camera.orientation, Vec3.multiply(2, {x: Math.sin(t), y: 0, z: Math.cos(t) })));
+];
+SHAPES.forEach(function(Name) { SHAPES[Name] = SHAPES[Name.toLowerCase()] = Name; });
+
+var shapes = SHAPES;
+
+// map shapes= querystring parameter
+if (kvargs.shapes)
+    shapes = kvargs.shapes.split(/\s*,\s*/).map(function(_name) {
+        var Name, name = _name.toLowerCase();
+        for(var i=0; i < SHAPES.length; i++) {
+            if (SHAPES[i].toLowerCase().indexOf(name) === 0) {
+                Name = SHAPES[i];
+                break;
+            }
+        }
+        if (!(Name in SHAPES))
+            print('example.js | WARNING: specified shape may not be valid...', _name, Name);
+        return Name;
+    }).filter(Boolean);
+
+var uuids = shapes.map(function(shape, i, arr) {
+    var deg = i/(arr.length) * 360;
+    return mkent(shape, Vec3.multiply(2, Quat.getFront(
+        Quat.multiply(MyAvatar.orientation, Quat.fromPitchYawRollDegrees(0,deg,0))
+    )));
 });
 
 Script.scriptEnding.connect(function() {
