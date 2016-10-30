92de0db0441ea719d92d1bf6bc02229ea134e4fa
humbletim <humbletim@gmail.com>
2016-10-29 22:38:30 -0400

    add $url parsing

--- snippets/extract-parameters.js
+++ snippets/extract-parameters.js
@@ -20,6 +20,9 @@ function extract_parameters_js_example() {
     extractParameters('debug=false', values);
 
     print('extract_parameters_js_example', JSON.stringify(values, 0, 2));
+    print('extract_parameters_js_example.$object', JSON.stringify(values.$object, 0, 2));
+    print('extract_parameters_js_example.$array', JSON.stringify(values.$array, 0, 2));
+    print('extract_parameters_js_example.$url', JSON.stringify(values.$url, 0, 2));
     return values;
 }
 
@@ -32,6 +35,7 @@ function extract_parameters_js_example() {
 //      "something.js": "",
 //      "$object": { "b": { "x": 5, "y": "asdf" } },
 //      "$array": { "0": [ "something.js" ], /*...*/, "a": [ "A/" ], "b": [ "{\"x\":5}", "{\"y\":\"asdf\"}" ] },
+//      "$url": { ...}
 //  }
 function extractParameters(url, mergeInto) {
     mergeInto = mergeInto || {};
@@ -40,38 +44,76 @@ function extractParameters(url, mergeInto) {
     if (!mergeInto.$array)
         Object.defineProperty(mergeInto, '$array', { value: {} });
 
-    function _merge(k,v) {
-        mergeInto[k] = v;
+    function _mergeArray(k,v) {
         mergeInto.$array[k] = mergeInto.$array[k] || [];
         mergeInto.$array[k].push(v);
+    }
+
+    function _merge(k,v) {
+        mergeInto[k] = v;
+        _mergeArray(k,v);
         try {
             var parsed = JSON.parse(v);
-            if (parsed && typeof parsed === 'object') {
-                mergeInto.$object[k] = mergeInto.$object[k] || (Array.isArray(parsed) ? [] : {});
-                for(var p in parsed)
-                    mergeInto.$object[k][p] = parsed[p];
+            if (parsed !== undefined) {
+                if (parsed && typeof parsed === 'object') {
+                    mergeInto.$object[k] = mergeInto.$object[k] || (Array.isArray(parsed) ? [] : {});
+                    for(var p in parsed)
+                        mergeInto.$object[k][p] = parsed[p];
+                } else
+                    mergeInto.$object[k] = parsed;
             }
         } catch(e) {}
     }
 
-    url
+    var length = url
         .split(/[&#?]/)
-        .filter(Boolean)
-        .forEach(function(kvp, i) {
-            if (!~kvp.indexOf('=')) {
-                _merge(kvp, undefined);
-                _merge(i, kvp);
-            }
+        .map(function(kvp, i) {
+            if (!kvp.length)
+                return _mergeArray(i, kvp);
             var arr = kvp.split('='),
                 k = unescape(arr.shift()).replace('[]', ''),
                 v = unescape(arr.join('='));
             _merge(k,v);
-        });
+
+            if (!~kvp.indexOf('='))
+                _merge(kvp, undefined);
+            _mergeArray(i, kvp);
+        }).length;
+
+    if (!('length' in mergeInto.$array))
+        Object.defineProperty(mergeInto.$array, 'length', { value: length })
+
+    if (!mergeInto.$url) {
+        var parts = url.match(/^((\w+:)[\/][\/]([^\/#?]+))(.*)$/)||['','','','',''],
+            protocol = parts[2] || url.split(':',1)[0]+':',
+            hostname = parts[3].split(':')[0],
+            port = parts[3].split(':')[1] || '',
+            host = parts[3],
+            origin = parts[1] || null,
+            pathname = parts[4].split(/[#?]/)[0] || url.replace(/^\w+:(?:\/\/)?/,'').split(/[?#]/)[0],
+            search = (url.match(/[?].*$/)||[''])[0].split('#')[0],
+            hash = (url.match(/#.*$/)||[])[0] || '';
+
+        Object.defineProperty(mergeInto, '$url', { value: {
+            hash: hash,
+            search: search,
+            pathname: pathname,
+            port: port,
+            hostname: hostname,
+            host: host,
+            protocol: protocol,
+            origin: origin,
+            href: url,
+            dirname: pathname.replace(/[\/][^\/]+$/,''),
+            basename: pathname.replace(/^.*[\/]/,''),
+            extname: pathname.split('.').pop()
+        } });
+    }
     return mergeInto;
 }
 
 // exports
-try { var global = module.exports; } catch(e) { global = (1,eval)('this'); }
+try { var global = module.exports = extractParameters;  } catch(e) { global = (1,eval)('this'); }
 global.extractParameters = extractParameters;
 global.extract_parameters_js_example = extract_parameters_js_example;
 
