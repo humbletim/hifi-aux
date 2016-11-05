2b568269bc4b69d8bedffc438ca87739aef4e1a6
humbletim <humbletim@gmail.com>
2016-11-05 04:52:16 -0400

    add script cache 404 tests

--- tests/script-cache/test-client-404.js
+++ tests/script-cache/test-client-404.js
@@ -1,5 +1,3 @@
-print('BEFORE 404...');
-Script.include('https://httpbin.org/status/404?'+new Date().getTime().toString(36))
-// currently this line will never be hint
-print('//AFTER 404');
-// the client script will not be "stoppable" via Running Scripts... anymore either
+print('BEFORE HTTP 404 Not Found...');
+Script.include('https://httpbin.org/status/404?' + new Date().getTime().toString(36))
+print('//AFTER HTTP 404 Not Found.');
