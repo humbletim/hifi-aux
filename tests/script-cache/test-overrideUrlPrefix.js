aa27890c3100c18eed666e226de16900e2f69d55
humbletim <humbletim@gmail.com>
2016-11-06 16:43:41 -0500

    update overrideUrlPrefix test to hit branch with noticeable difference

--- tests/script-cache/test-overrideUrlPrefix.js
+++ tests/script-cache/test-overrideUrlPrefix.js
@@ -1,6 +1,7 @@
-// override local tutorial URLs to pull from github master
+// override local tutorial URLs to pull from a different branch
 Resources.overrideUrlPrefix(
     '/~/tutorials/',
-    'https://raw.githubusercontent.com/highfidelity/hifi/master/scripts/tutorials/'
+    'https://rawgit.com/humbletim/hifi/pull-8973-test/scripts/tutorials/'
+    //'https://raw.githubusercontent.com/highfidelity/hifi/master/scripts/tutorials/'
 );
 Script.include('/~/tutorials/createPingPongGun.js');
