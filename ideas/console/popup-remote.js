3643214bfb123ebe993eee12a9f2f52b9ea322a5
humbletim <humbletim@gmail.com>
2016-11-19 22:43:33 -0500

    add popup-*.js helper scripts

---
+++ ideas/console/popup-remote.js
@@ -0,0 +1,2 @@
+// this is just to make explaining usage easier... could load popup-console-window.js directly with params too
+Script.include(Script.resolvePath('').replace('popup-remote.js','popup-console-window.js')+'?global=console');
