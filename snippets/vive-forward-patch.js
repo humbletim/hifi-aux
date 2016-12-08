43776de8ee52682de85024f8e7ca96c88de3739d
humbletim <humbletim@gmail.com>
2016-12-07 20:12:21 -0500

    add comment header

--- snippets/vive-forward-patch.js
+++ snippets/vive-forward-patch.js
@@ -1,3 +1,14 @@
+// vive-forward-patch.js
+//
+//   Example Vive controller patch script for @XaosPrincess
+//   With "Advanced Movement For Hand Controllers" enabled,
+//    her left forward touchpad had a mechanical defect.
+//
+//   This script lets the LeftApplicationMenu button move forward instead.
+//   (Right controller menu button unaffected)
+//
+// --humbletim @ 2016.12.07
+
 var input = {
     "name": "Vive to Standard",
     "channels": [
